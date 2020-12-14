47be3e0e3feeb57f5226e003e4c39d06b7f57339
jespi116 <jespi116@fiu.edu>
2020-12-14 02:21:46 -0500

    comments implemented

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
